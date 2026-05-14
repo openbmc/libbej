622b0ad5b1641db440ff7ea903c89c7847c50a72
Konstantin Aladyshev <aladyshev22@gmail.com>
2026-05-14 12:12:09 +0300

    bej_decoder: Handle top level annotation flag
    If nested annotation is decoded and top level annotation flag is set
    property offset should be calculated from the start.
    
    Change-Id: I5b4ad0fcc04fc534c31bfa476d8bce2871bb5c92
    Signed-off-by: Konstantin Aladyshev <aladyshev22@gmail.com>

--- src/bej_decoder_core.c
+++ src/bej_decoder_core.c
@@ -186,7 +186,14 @@ static int bejGetDictionaryAndProperty(
     else if (schemaType == bejAnnotation)
     {
         *dictionary = params->annotDictionary;
-        dictPropOffset = params->state.annoDictPropOffset;
+        if (params->sflv.format.readOnlyPropertyAndTopLevelAnnotation)
+        {
+            dictPropOffset = bejDictGetFirstAnnotatedPropertyOffset();
+        }
+        else
+        {
+            dictPropOffset = params->state.annoDictPropOffset;
+        }
     }
     else
     {
